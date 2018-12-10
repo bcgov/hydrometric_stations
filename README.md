dee38fd848c3eda0c49164b2dcf8c20843122eed
Sam Albers <sam.albers@gov.bc.ca>
2018-12-10 15:09:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<!-- 
+<a id="devex-badge" rel="Delivery" href="https://github.com/BCDevExchange/assets/blob/master/README.md"><img alt="In production, but maybe in Alpha or Beta. Intended to persist and be supported." style="border-width:0" src="https://assets.bcdevexchange.org/images/badges/delivery.svg" title="In production, but maybe in Alpha or Beta. Intended to persist and be supported." /></a>
+-->
+
+hydrometric_stations
+============================
+
+### Usage
+
+Process used to create the [*Hydrometric Stations - Active and Discontinued*](https://catalogue.data.gov.bc.ca/dataset/hydrometric-stations-active-and-discontinued) data object in the [BC Data Catalogue](https://catalogue.data.gov.bc.ca/dataset). The repository outlines the process used in R with the [tidyhydat](https://CRAN.R-project.org/package=tidyhydat) package to create the data object. 
+
+### Project Status
+Updated with each quarterly release of [HYDAT](https://ec.gc.ca/rhc-wsc/default.asp?n=9018B5EC-1), the Environment and Climate Change Canada database of hydrometric data.  
+
+### Getting Help or Reporting an Issue
+
+To report bugs/issues/feature requests, please file an [issue](https://github.com/bcgov/hydrometric_stations/issues/).
+
+### How to Contribute
+
+If you would like to contribute, please see our [CONTRIBUTING](CONTRIBUTING.md) guidelines.
+
+Please note that this project is released with a [Contributor Code of Conduct](CODE_OF_CONDUCT.md). By participating in this project you agree to abide by its terms.
+
+### License
+
+```
+Copyright 2018 Province of British Columbia
+
+Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+```
+---
+*This project was created using the [bcgovr](https://github.com/bcgov/bcgovr) package.* 
---
+++ create_record.R
@@ -0,0 +1,106 @@
+# Copyright 2018 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(sf)
+library(bcmaps)
+library(tidyhydat)
+
+
+# Functions ---------------------------------------------------------------
+
+## Create realtime url
+real_url <- function(station_number){
+  paste0("https://wateroffice.ec.gc.ca/report/real_time_e.html?stn=", station_number)
+}
+
+## Create archival url
+archive_url <- function(station_number){
+  paste0("https://wateroffice.ec.gc.ca/report/historical_e.html?stn=", station_number)
+}
+
+
+# Get data ----------------------------------------------------------------
+
+#bc_stns <- tidyhydat::hy_stations(prov_terr_state_loc = "BC")
+## Using allstations here
+bc_stns <- filter(allstations, PROV_TERR_STATE_LOC == "BC")
+
+## Finding the date range of the data for all measurement (level, flow etc)
+
+## This will take quite some time to run. (~5 minutes)
+full_date_range <- hy_daily(prov_terr_state_loc = "BC") %>%
+  filter(!is.na(Value)) %>%
+  group_by(STATION_NUMBER) %>%
+  summarise(DATE_FROM = min(Date), DATE_TO = max(Date)) %>%
+  mutate(DATE_FROM = as.character(as.Date(DATE_FROM, "%m/%d/%Y"), "%Y%m%d") ) %>%
+  mutate(DATE_TO = as.character(as.Date(DATE_TO, "%m/%d/%Y"), "%Y%m%d") ) ## date requested in YYYYMMDD
+
+## Regulation data
+stn_regulation <- hy_stn_regulation(prov_terr_state_loc = "BC") %>%
+  mutate(REGULATED = case_when(
+    REGULATED == FALSE ~ "NATURAL",
+    REGULATED == TRUE ~ "REGULATED"
+    ))
+
+# Add relevant columns ----------------------------------------------------
+
+## Columns to do/ filled with NA's:
+## STREAM_ORD
+## CAPTR_SCAL
+## WATRSHD_ID
+## WTr_GRP_CD
+## GEOMETRY
+
+## Columns with Uncertain Status
+# HDRMTRCSTT
+# OBJECTID
+
+## Be nice to find a way to test if the urls work
+## A faulty url like this: https://wateroffice.ec.gc.ca/report/real_time_e.html?stn=13EA004 just directs to the search page
+
+## Assign stations to drainages
+bc_stns <- bc_stns %>%
+  mutate(REALTM_URL = case_when(
+    REAL_TIME == TRUE ~ real_url(STATION_NUMBER)
+  )) %>% ## generate realtime links
+  mutate(ARCHIV_URL = archive_url(STATION_NUMBER)) %>% ## generate archival links
+  mutate(FEAT_CODE = "CF29300000") %>%
+  left_join(select(stn_regulation, STATION_NUMBER, REGULATED)) %>%
+  left_join(select(full_date_range, STATION_NUMBER, DATE_FROM, DATE_TO)) %>%
+  mutate(WATRSHD_ID = "NA", HDRMTRCSTT = "NA", WTr_GRP_CD = "NA", STREAM_ORD = "NA", GEOMETRY = "NA" ,
+         OBJECTID = "NA", CAPTR_SCAL = "NA") %>%
+  select(WATRSHD_ID, DATE_FROM, HDRMTRCSTT, WTr_GRP_CD, STREAM_ORD, HYD_STATUS, REAL_TIME, DATE_TO, REALTM_URL,
+         ARCHIV_URL, REGULATED, GEOMETRY, OBJECTID, STATION_NUMBER, CAPTR_SCAL, STATION_NAME, FEAT_CODE, LATITUDE, LONGITUDE)
+
+colnames(bc_stns) <- c("WATRSHD_ID", "START_DATE", "HDRMTRCSTT", "WTr_GRP_CD", "STREAM_ORD",
+                       "STN_OP_STA", "STN_RT_STA", "END_DATE", "REALTM_URL", "ARCHIV_URL", "FLOW_TYPE",
+                       "GEOMETRY", "OBJECTID", "STATION_NO", "CAPTR_SCAL", "STATN_NAME",
+                       "FEAT_CODE", "X", "Y")
+
+
+
+## Output data to keep a record of the data history
+readr::write_csv(bc_stns, paste0("previous_records/bc_hydrometric_",Sys.Date(),".csv"))
+
+## Output .csv file
+readr::write_csv(bc_stns, "bc_hydrometric.csv")
+
+
+
+
+
+
